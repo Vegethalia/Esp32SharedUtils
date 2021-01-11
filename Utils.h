24543b706d9ff0ac1065771dd127f4e208344b28
Jordi Carrión <vegethalia@pleyhades.com>
2021-01-11 23:30:24 +0100

    Utils - Added method to calculate the distance (in meters) between two GPS coordinates (in degrees)

--- Utils.h
+++ Utils.h
@@ -1,7 +1,7 @@
 #include <string>
 #include <math.h>
 
-#define EARTH_RADIUS_METERS 6371000
+#define EARTH_RADIUS_METERS 6372795.0f//6371000
 
 class Utils {
 public:
@@ -14,7 +14,9 @@ public:
 		return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 	}
 
-	static float DistanceBetween2Points(float Lat1, float Lon1, float Lat2, float Lon2, float unit_conversion)
+	//Returns the distance (in meters) between two GPS coordinates. Uses the ‘haversine’ formula.
+	//Based on https://www.movable-type.co.uk/scripts/latlong.html by Chris Veness
+	static float DistanceBetween2Points(float Lat1, float Lon1, float Lat2, float Lon2)
 	{
 		float dLat = radians(Lat2 - Lat1);
 		float dLon = radians(Lon2 - Lon1);
@@ -25,7 +27,26 @@ public:
 
 		float d = 2.0f * atan2(sqrt(a), sqrt(1.0f - a));
 
-		return d * EARTH_RADIUS_METERS * unit_conversion;
+		return d * EARTH_RADIUS_METERS;
+	}
+
+	//Same as above, but faster and less precise (47.78m vs 61.95m for instance). Uses the Equirectangular approximation formula.
+	//If performance is an issue and precission is not important...
+	//Based on https://www.movable-type.co.uk/scripts/latlong.html by Chris Veness
+	static float FastDistanceBetween2Points(float Lat1, float Lon1, float Lat2, float Lon2)
+	{
+//		const x = (λ2 - λ1) * Math.cos((φ1 + φ2) / 2);
+//		const y = (φ2 - φ1);
+//		const d = Math.sqrt(x * x + y * y) * R;
+
+		float dLat = radians(Lat2 - Lat1);
+		float dLatAdd = radians(Lon2 + Lon1);
+		float dLon = radians(Lon2 - Lon1);
+
+		float x = dLon * cos(dLatAdd/2);
+		float d = sqrt(x * x + dLat * dLat) * EARTH_RADIUS_METERS;
+
+		return d;
 	}
 
 
